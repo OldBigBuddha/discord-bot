4f7b7feec0400a0958924f5fbb3248772165fef0
Shinra Yamakawa <13755428+OldBigBuddha@users.noreply.github.com>
2023-10-23 13:36:05 +0900

    chore: use `DENO_ENV`

--- bot/logger.ts
+++ bot/logger.ts
@@ -1,3 +1,21 @@
 import { Logger } from "tslog";
 
-export const botLogger = new Logger({ name: "bot" });
+const DENO_ENV = Deno.env.get("DENO_ENV");
+
+function isProduction(): boolean {
+  return DENO_ENV === "production";
+}
+
+function calcMinLevel(): number {
+  if (isProduction()) {
+    return 3; // info
+  }
+
+  return 1; // silly
+}
+
+export const botLogger = new Logger({
+  name: "bot",
+  minLevel: calcMinLevel(),
+  type: isProduction() ? "json" : "pretty",
+});
