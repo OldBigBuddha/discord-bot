fdd31957c3af888b91e20c75e442b49b12b263b0
Shinra Yamakawa <13755428+OldBigBuddha@users.noreply.github.com>
2023-12-15 01:15:07 +0900

    chore: setup for bun

--- utils/env.ts
+++
@@ -1,9 +0,0 @@
-const DENO_ENV = Deno.env.get("DENO_ENV");
-
-/**
- * 本番環境かそうでないかを確認する
- * @returns 本番環境であれば true
- */
-export function isProduction(): boolean {
-  return DENO_ENV === "production";
-}
